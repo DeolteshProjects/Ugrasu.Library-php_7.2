67acd11f77e08f03d8f985d77c57bf51127b3a60
Denis Shilenkov <deoltesh.projects@yandex.ru>
2019-03-29 04:48:57 +0500

    Добавлена стартовая страница и страница разлогирования

--- system/modules/fast_auth.php
+++ system/modules/fast_auth.php
@@ -4,12 +4,15 @@ if (!empty($_POST['access'])) {
     switch ($_POST['access']) {
         case "T":
             $_SESSION['access'] = "Преподаватель";
+            $_SESSION['username'] = "Феногенов Антон";
             break;
         case "R":
             $_SESSION['access'] = "Руководитель ОПОП";
+            $_SESSION['username'] = "Русанов Михаил";
             break;
         case "L":
             $_SESSION['access'] = "Библиотека";
+            $_SESSION['username'] = "Работник библиотеки";
             break;
         default:
             exit();
